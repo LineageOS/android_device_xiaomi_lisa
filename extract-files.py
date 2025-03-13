7829f30911d5e2696caa87fb599cbed71ba20f25
Bruno Martins <bgcngm@gmail.com>
2025-03-13 21:37:39 +0000

    lisa: Shim camera libs
    Change-Id: Id4bec2bc8bf5da014a69e1953bdb36bc85d11f1e

--- extract-files.py
+++ extract-files.py
@@ -33,9 +33,12 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/etc/camera/pureShot_parameter.xml', 'vendor/etc/camera/pureView_parameter.xml'): blob_fixup()
         .regex_replace(r'=(\d+)>', r'="\1">'),
     'vendor/lib64/hw/camera.qcom.so': blob_fixup()
-        .binary_regex_replace(b'\x73\x74\x5F\x6C\x69\x63\x65\x6E\x73\x65\x2E\x6C\x69\x63', b'\x63\x61\x6D\x65\x72\x61\x5F\x63\x6E\x66\x2E\x74\x78\x74'),
+        .binary_regex_replace(b'\x73\x74\x5F\x6C\x69\x63\x65\x6E\x73\x65\x2E\x6C\x69\x63', b'\x63\x61\x6D\x65\x72\x61\x5F\x63\x6E\x66\x2E\x74\x78\x74')
+        .add_needed('libprocessgroup_shim.so'),
     'vendor/lib64/hw/camera.xiaomi.so': blob_fixup()
         .sig_replace('29 07 00 94', '1F 20 03 D5'),
+    'vendor/lib64/hw/com.qti.chi.override.so' : blob_fixup()
+        .add_needed('libprocessgroup_shim.so'),
 }  # fmt: skip
 
 
