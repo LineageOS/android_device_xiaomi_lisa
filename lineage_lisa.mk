71e57a73b3d6f1e464340ce27d825c43dc0331e7
Giovanni Ricca <giovanniricca@proton.me>
2025-05-21 12:32:31 +0200

    lisa: Update blobs to OS2.0.1.0.UKOMIXM
    Change-Id: I86adb04eb25d7b76009bd106945d033ec2660810

--- lineage_lisa.mk
+++ lineage_lisa.mk
@@ -19,5 +19,5 @@ PRODUCT_NAME := lineage_lisa
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="lisa_global-user 14 UKQ1.231108.001 V816.0.10.0.UKOMIXM release-keys" \
-    BuildFingerprint=Xiaomi/lisa_global/lisa:14/UKQ1.231108.001/V816.0.10.0.UKOMIXM:user/release-keys
+    BuildDesc="lisa_global-user 14 UKQ1.240624.001 OS2.0.1.0.UKOMIXM release-keys" \
+    BuildFingerprint=Xiaomi/lisa_global/lisa:14/UKQ1.240624.001/OS2.0.1.0.UKOMIXM:user/release-keys
