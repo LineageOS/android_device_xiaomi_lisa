887df74fdb42f1933e410f6a127296db6bc8f8a6
Giovanni Ricca <giovanniricca@proton.me>
2025-07-14 10:25:04 +0200

    lisa: rro_overlays: Move to OverlayConfig
    Since Android 11, the recommended approach for handling RROs is
    OverlayConfig [1].
    
    Also move the device-specific overlays to the odm partition, because
    they should belong there
    
    [1]:
    https://source.android.com/docs/core/runtime/rros#configuring-overlays
    
    Co-authored-by: Alexander Winkowski <dereference23@outlook.com>
    Change-Id: I4beec15cfa89c88a7ab672af65c9d9f6c358f441

--- device.mk
+++ device.mk
@@ -21,6 +21,9 @@ PRODUCT_PACKAGES += \
     WifiOverlayLisaGL \
     WifiOverlayLisaIN
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rro_overlays/config-odm.xml:$(TARGET_COPY_OUT_ODM)/overlay/config/config.xml
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
